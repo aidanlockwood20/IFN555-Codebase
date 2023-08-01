64960314ba8f71d7fcc9e3fa5a76654d686e89da
Aidan Lockwood <aidanlockwood@yahoo.com.au>
2023-08-01 18:42:21 +1000

    First Commit

---
+++ Week1/Week1/AddingComments.cs
@@ -0,0 +1,14 @@
+﻿namespace Week1
+{
+    public class AddingComments
+    {
+        public static void WriteNewCodeComment(string[] args)
+        {
+            Console.WriteLine("Program comments are nonexecuting lines of code");
+            Console.WriteLine("that can help document a program");
+
+            // end of message
+        }
+    }
+}
+
---
+++ Week1/Week1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Week1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello IFN555 Class!");
+			StopSign newStopSign = new StopSign.CreateStopSign();
+		}
+	}
+}
+
